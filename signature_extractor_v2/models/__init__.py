1306bded6964a919c0e96c2cd3843ace4e30b0a0
Lina-go <l.gomez1@uniandes.edu.co>
2025-09-19 23:04:59 -0500

    changes

--- signature_extractor_v2/models/__init__.py
+++ signature_extractor_v2/models/__init__.py
@@ -2,19 +2,19 @@
 Data models and structures for signature extraction.
 """
 
-from .extraction_result import ExtractionResult, SourceResult, ProcessingSummary
-from .signature_data import SignatureData, SignatureMetadata, SignatoryInfo, DateInfo
-from .document_metadata import DocumentMetadata, PageInfo, ProcessingStats
+#from .extraction_result import ExtractionResult, SourceResult, ProcessingSummary
+#from .signature_data import SignatureData, SignatureMetadata, SignatoryInfo, DateInfo
+#from .document_metadata import DocumentMetadata, PageInfo, ProcessingStats
 
 __all__ = [
-    "ExtractionResult",
-    "SourceResult", 
-    "ProcessingSummary",
-    "SignatureData",
-    "SignatureMetadata",
-    "SignatoryInfo",
-    "DateInfo",
-    "DocumentMetadata",
-    "PageInfo",
-    "ProcessingStats"
+#    "ExtractionResult",
+#    "SourceResult", 
+#    "ProcessingSummary",
+#    "SignatureData",
+#    "SignatureMetadata",
+#    "SignatoryInfo",
+#    "DateInfo",
+#    "DocumentMetadata",
+#    "PageInfo",
+#    "ProcessingStats"
 ]
--- signature_extractor_v2/utils/__init__.py
+++ signature_extractor_v2/utils/__init__.py
@@ -5,7 +5,6 @@ Utility modules for the signature extraction framework.
 from .progress_tracker import ProgressTracker
 from .logging_config import setup_logging, get_logger
 from .prompt_loader import load_prompt, get_available_prompts
-from .pdf_converter import convert_pdf_to_images, save_page_image, get_pdf_info
 from .llm_interface import LLMInterface, create_llm_interface, get_available_providers
 
 __all__ = [
@@ -14,9 +13,6 @@ __all__ = [
     "get_logger",
     "load_prompt",
     "get_available_prompts",
-    "convert_pdf_to_images",
-    "save_page_image", 
-    "get_pdf_info",
     "LLMInterface",
     "create_llm_interface",
     "get_available_providers"
